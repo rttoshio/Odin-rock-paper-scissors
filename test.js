a5c31d84a0bb0786391cf2333ff8632fbea1a3a9
rttoshio <rttoshio@hotmail.com>
2022-04-07 00:49:16 -0300

    added function computerPlay (5)

--- test.js
+++ test.js
@@ -1,4 +1,26 @@
 function computerPlay () {
-    return Math.floor(Math.random() * 3)
-    
+    const randomPlay =  Math.floor(Math.random() * 3);
+    if (randomPlay === 0) {
+        return "Rock";
+    } else if (randomPlay === 1) {
+        return "Paper";
+    } else if (randomPlay === 2) {
+        return "Scissors";
+
+}
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection;
+  let cpu = computerSelection;
+    if (playerSelection == computerSelection) {
+        return "Draw";
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        return "You Lose! Rock beats Paper"
+    } else {
+        return "You Win!" + player + " beats " cpu
+    }
 }
