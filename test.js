77501c29d1925401cf3c778cdeda579d8a5b2c4a
rttoshio <rttoshio@hotmail.com>
2022-04-07 16:41:50 -0300

    added loop for 5 rounds and the score counter and changed changed some syntax

--- test.js
+++ test.js
@@ -6,21 +6,37 @@ function computerPlay () {
         return "Paper";
     } else if (randomPlay === 2) {
         return "Scissors";
-
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
-  let player = playerSelection;
+  let player = playerSelection.toLowerCase()
   let cpu = computerSelection;
+
     if (playerSelection == computerSelection) {
         return "Draw";
     } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        cpuScore++;
         return "You Lose! Paper beats Rock";
     } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        cpuScore++;
         return "You Lose! Scissors beats Paper";
     } else if (playerSelection == "scissors" && computerSelection == "Rock") {
-        return "You Lose! Rock beats Paper"
+        cpuScore++;
+        return "You Lose! Rock beats Paper";
     } else {
-        return "You Win!" + player + " beats " cpu
+        return "You Win!" + player + " beats " + cpu;
+        playerScore++;
     }
 }
+
+playerSelection[0].toUpperCase + playerSelection.slice(1).toLowerCase() // **case insensitive
+
+let playerScore = 0;
+let cpuScore = 0;
+
+for(var i=0;i<5;i++){
+    let playerSelection = (window.prompt("Jo-ken-Po", ""));
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection))
+  }
