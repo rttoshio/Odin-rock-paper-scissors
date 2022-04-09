ea0215c15360b05025c002dbfb22be3df4170600
rttoshio <rttoshio@hotmail.com>
2022-04-09 16:54:40 -0300

    fixed some syntax errors from last push

--- test.js
+++ test.js
@@ -1,41 +1,38 @@
 function computerPlay () {
     const randomPlay =  Math.floor(Math.random() * 3);
     if (randomPlay === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomPlay === 1) {
-        return "Paper";
+        return "paper";
     } else if (randomPlay === 2) {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-  let player = playerSelection.toLowerCase()
-  let cpu = computerSelection;
-
-    if (playerSelection == computerSelection) {
-        return "Draw";
-    } else if (playerSelection == "rock" && computerSelection == "Paper") {
-        cpuScore++;
-        return "You Lose! Paper beats Rock";
-    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
-        cpuScore++;
-        return "You Lose! Scissors beats Paper";
-    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
-        cpuScore++;
-        return "You Lose! Rock beats Paper";
-    } else {
-        return "You Win!" + player + " beats " + cpu;
-        playerScore++;
-    }
-}
+    playerSelection = playerSelection.toLowerCase();
+     let cpu = computerSelection;
+   
+       if (playerSelection == computerSelection) {
+           return "Draw";
+       } else if (playerSelection == "rock" && computerSelection == "paper") {
+           return "You Lose! Paper beats Rock";
+       } else if (playerSelection == "paper" && computerSelection == "scissors") {
+           return "You Lose! Scissors beats Paper";
+       } else if (playerSelection == "scissors" && computerSelection == "rock") {
+           return "You Lose! Rock beats Paper";
+       } else {
+           return "You Win!" + playerSelection[0].toUpperCase()+playerSelection.slice(1) + " beats " + cpu[0].toUpperCase()+cpu.slice(1)
+       }
+   }
+   
 
 playerSelection[0].toUpperCase + playerSelection.slice(1).toLowerCase() // **case insensitive
 
 let playerScore = 0;
 let cpuScore = 0;
 
-for(var i=0;i<5;i++){
+for (var i=0;i<5;i++){
     let playerSelection = (window.prompt("Jo-ken-Po", ""));
     const computerSelection = computerPlay()
     console.log(playRound(playerSelection, computerSelection))
